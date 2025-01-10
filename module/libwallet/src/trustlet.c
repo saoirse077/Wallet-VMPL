5c7f085021b9e7b4b398c6829bf23b951875ace8
Masanori Misono <m.misono760@gmail.com>
2025-01-10 22:43:11 +0000

    module: dir read

--- module/libwallet/src/trustlet.c
+++ module/libwallet/src/trustlet.c
@@ -9,9 +9,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
 #include "memory.h"
 #include "vmpl.h"
+
+#include <sys/syscall.h>
+#include <dirent.h>
+struct linux_dirent64 {
+    uint64_t        d_ino;    /* 64-bit inode number */
+    uint64_t        d_off;    /* Not an offset; see getdents() */
+    unsigned short  d_reclen; /* Size of this dirent */
+    unsigned char   d_type;   /* File type */
+    char            d_name[]; /* Filename (null-terminated) */
+};
 
 #define INIT_CREATE_TRUSTLET(id, data, size)\
     do {                                    \
@@ -20,6 +29,10 @@
     call.trustlet.trustlet_data = data;     \
     call.trustlet.size = size;              \
     } while(0)
+
+static int is_dot_or_dotdot(const char *name) {
+    return name[0] == '.' && (name[1] == '\0' || (name[1] == '.' && name[2] == '\0'));
+}
 
 static void allocate_trustlet_struct(struct trustlet_invokation** z){
     uint8_t* buf = aligned_alloc(4096, 4096);
@@ -132,7 +145,58 @@ retry:
         if (count > sizeof(arg->read.buf)) {
             count = sizeof(arg->read.buf);
         }
-        int read_bytes= pread(fd, read_buffer, count, arg->read.offset);
+        int read_bytes = 0;
+
+        if (arg->read.offset == (uint64_t)-1) {
+            // dir read
+            // XXX: code adopted from read_dir() of linux pal of gramine
+            char *buf = read_buffer;
+            char dirbuf[1024];
+            // gcc does not provide a wrapper for getdents64
+            int size = syscall(SYS_getdents64, fd, dirbuf, sizeof(dirbuf));
+            if (size == -1) {
+                printf("Failed to read dir!\n");
+                // TODO: handle error
+            } else {
+                char *ptr = dirbuf;
+                int remaining = size;
+                printf("Guest requet: dire_read: %d bytes for dentry\n", size);
+                while(remaining > 0) {
+                    struct linux_dirent64* dirent = (struct linux_dirent64*)ptr;
+                    if (is_dot_or_dotdot(dirent->d_name)) {
+                        goto skip;
+                    }
+
+                    int is_dir = dirent->d_type == DT_DIR;
+                    size_t len = strlen(dirent->d_name);
+
+                    if (len + 1 + (is_dir ? 1 : 0) > count) {
+                        // seek back fd so that next read will read the same dentry
+                        lseek(fd, -remaining, SEEK_CUR);
+                        break;
+                    }
+
+                    memcpy(buf, dirent->d_name, len);
+                    if (is_dir) {
+                        buf[len++] = '/';
+                    }
+                    buf[len++] = '\0';
+
+                    buf += len;
+                    read_bytes += len;
+                    count -= len;
+                    if (count == 0) {
+                        break;
+                    }
+skip:
+                    ptr += dirent->d_reclen;
+                    remaining -= dirent->d_reclen;
+                }
+            }
+        } else {
+            // file read
+            read_bytes = pread(fd, read_buffer, count, arg->read.offset);
+        }
         arg->read.count = read_bytes;
         invoke_data->invokation_type = requestRead;
         printf("Guest request: read: fd=%d, offset=%ld, count=%lu\n", fd, arg->read.offset, arg->read.count);
