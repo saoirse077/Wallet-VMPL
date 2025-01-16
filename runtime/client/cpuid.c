93aad3a28fc0a379e0aba8eb8540b17e8f255e8d
Patrick Sabanic <patrick.sabanic@tum.de>
2025-01-16 11:23:50 +0000

    Update cpuid.c

--- runtime/client/cpuid.c
+++ runtime/client/cpuid.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 
 void notify_monitor(){
     __asm__ volatile("mov $0x4FFFFFF8, %%rax; cpuid":::"rax", "rbx", "rcx", "rdx");
@@ -9,4 +10,12 @@ void call_outb(){
 
 void trustlet_exit() {
     __asm__ volatile("mov $0x4FFFFFA1, %%rax; cpuid":::"rax", "rbx", "rcx", "rdx");
+}
+
+void call_outb_with_value(uint64_t value) {
+   __asm__ volatile("mov $0x4FFFFFA2, %%rax; mov %0, %%rcx; cpuid":: "r" (value) :"rax", "rbx", "rcx", "rdx");
+}
+
+void resize_channel(uint64_t select, uint64_t size) {
+    __asm__ volatile("mov $0x4FFFFFA1, %%rax; mov %0, %%rcx; mov %1, %%rdx; cpuid":: "r" (select), "r" (size):"rax", "rbx", "rcx", "rdx");
 }
