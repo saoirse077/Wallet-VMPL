a645e4521a0456f0dc99117ab69806f520511ae1
Michael Hackl <michael.hackl@tum.de>
2025-01-23 01:24:16 +0000

    update

--- Benchmarks/sebs_script.sh
+++ Benchmarks/sebs_script.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -eux
 
-# (cd module; make -C libwallet/ clean; make libwallet/libwallet.a NODEBUG=1; make vmpl.ko; insmod vmpl.ko; rm -r python/build)
-(cd module; insmod vmpl.ko)
-# apt install -y build-essential python3-dev python3-venv python3-pip docker.io jq
+(cd module; make clean; make -C libwallet/ clean; rm -r python/build || true)
+docker volume prune -f
+
+(cd module; make libwallet/libwallet.a NODEBUG=1; make vmpl.ko; insmod vmpl.ko)
+apt install -y build-essential python3-dev python3-venv python3-pip docker.io jq
+
 cd Benchmarks/SeBS/
-# ./install.py --no-aws --azure --no-gcp --no-openwhisk --local
+rm -r cache/ || true
+
+./install.py --no-aws --azure --no-gcp --no-openwhisk --local
 source python-venv/bin/activate
-# pip3 install ../../module/python
-# tools/build_docker_images.py --deployment 'wallet' --language 'python' --language-version '3.11'
+pip3 install ../../module/python
+
+tools/build_docker_images.py --deployment 'wallet' --language 'python' --language-version '3.11'
 ./sebs.py storage start minio --port '9011' --output-json 'out_storage.json'
-rm -r cache/
 
 jq ".deployment.wallet.storage = input | .experiments.\"perf-cost\".benchmark = \"$1\"" 'config/config_template.json' 'out_storage.json' > 'config/config.json'
 
