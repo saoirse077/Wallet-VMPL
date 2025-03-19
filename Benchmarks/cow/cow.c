45524a7a6929154b4fdf137d60dcd1a95182ecc2
Patrick Sabanic <patrick.sabanic@tum.de>
2025-03-19 15:18:00 +0000

    Add simple CoW test

--- Benchmarks/cow/cow.c
+++ Benchmarks/cow/cow.c
@@ -6,7 +6,7 @@
 int main() {
 
   int* a = malloc(64);
-  fprintf(stderr, "Before finalize");
+  fprintf(stderr, "Before finalize: %p", a);
 
   __asm__ volatile("mov $0x4FFFFFF4, %%rax; cpuid":::"rax", "rbx", "rcx", "rdx");
 
@@ -14,6 +14,9 @@ int main() {
   *a = 5;
 
   fprintf(stderr, "Test print");
+  notify_monitor();
+
+  fprintf(stderr, "Second try");
   trustlet_exit();
 
   return 0;
--- Benchmarks/cow/run.py
+++ Benchmarks/cow/run.py
@@ -1,5 +1,6 @@
 import ctypes
 import sys
+import time
 lib = ctypes.CDLL("../../module/libwallet/libwallet.so")
 print(lib.monitor_connect())
 lib.create_zygote.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p)
@@ -11,9 +12,12 @@ lib.create_channel.argtypes = (ctypes.c_int, ctypes.c_int)
 zid1 = lib.create_zygote(b"libpal.so", b"manifest", b"libsysdb.so")
 func = b""
 tid1 = lib.create_trustlet(zid1, func)
-
+tid2 = lib.create_trustlet(zid1, func)
+time.sleep(5)
 input_data = b"a"
 output_size = 1
+print("First")
 output1 = lib.invoke_trustlet(tid1, input_data, output_size)
-
-#output1 = lib.invoke_trustlet(tid1, input_data, output_size)
+#time.sleep(5)
+print("Second")
+output1 = lib.invoke_trustlet(tid2, input_data, output_size)
