29c714238aa913448d5958d34032ab5a2ea23e45
Patrick Sabanic <patrick.sabanic@tum.de>
2025-04-07 17:50:11 +0000

    Update results

--- Benchmarks/scale/run.py
+++ Benchmarks/scale/run.py
@@ -6,10 +6,13 @@ from tempfile import TemporaryDirectory
 from pathlib import Path
 import subprocess
 import time
+import resource
 
 sys.path.append("../CVM_eval/")
 sys.path.append("../CVM_eval/tasks")
 
+# resource.setrlimit(resource.RLIMIT_NPROC, (163840,163840))
+# print(f"Set limit: {resource.getrlimit(resource.RLIMIT_NPROC)}")
 from tasks.vm import (
     VMResource,
     get_vm_resource,
@@ -19,17 +22,31 @@ from tasks.vm import (
 from tasks.qemu import spawn_qemu
 from tasks.config import SSH_PORT
 
-from multiprocessing import Process, Lock, Array
+from multiprocessing import Process, Lock, Array, Value
 
-out = open("result.csv", "w")
+
+result_file = Path("result.csv")
+if not result_file.is_file():
+    out = open("result.csv", "w")
+    out.write("type,instances,assigned_memory,memory_usage,shared_pages\n")
+    out.flush()
+else:
+    out = open("result.csv", "a")
+
 vm_type = "vm"
 mem = 0.5
 
 import docker
 
 
-def spawn_container(i, arr, l):
+def get_shared():
+    with open("/sys/kernel/mm/ksm/pages_shared", "r") as f:
+        return int(f.read())
+
+
+def spawn_containers(i, arr, l, value):
     client = docker.from_env()
+    containers = []
     env = {
         "CONTAINER_GID": str(os.getgid()),
         "CONTAINER_UID": str(os.getuid()),
@@ -45,58 +62,96 @@ def spawn_container(i, arr, l):
         stderr=True,
         detach=True,
     )
+    print(f"Started container {i}")
+    time.sleep(10)
+    container.reload()
+
+    cid = container.id
+
+    if "running" in container.status:
+        with open(f"/sys/fs/cgroup/system.slice:docker:{cid}/memory.peak") as f:
+            peak_memory = int(f.read())
+        arr[i] = peak_memory
     l.acquire()
     l.release()
     container.reload()
+
     if not "running" in container.status:
         arr[i] = 0
         container.stop()
         return
 
-    cid = container.id
     with open(f"/sys/fs/cgroup/system.slice:docker:{cid}/memory.peak") as f:
         peak_memory = int(f.read())
     arr[i] = peak_memory
 
     container.stop()
+    client.close()
 
 
-def run_kata(n=1):
+def run_kata(n=[1]):
     lock = Lock()
     lock.acquire()
-    result_array = Array("i", range(n))
+    result_array = Array("i", range(n[-1]))
+    mem_total = Value("i", 0)
     process_list = []
-    for i in range(n):
-        p = Process(target=spawn_container, args=(i, result_array, lock))
-        process_list.append(p)
-    print(f"Starting {n} containers")
-    for p in process_list:
-        p.start()
-    time.sleep(20)
+    exec_list = []
+    for i in range(len(n)):
+        if i == 0:
+            exec_list.append(n[i])
+        else:
+            exec_list.append(n[i] - n[i - 1])
+    print(f"VM cycles: {exec_list}")
+    offset = 0
+    for vms in exec_list:
+        if offset != 0:
+            print(f"Starting {vms} additional VMs")
+        else:
+            print(f"Starting {vms} VMs")
+
+        for i in range(offset, offset + vms, 1):
+            p = Process(
+                target=spawn_containers, args=(i, result_array, lock, mem_total)
+            )
+            process_list.append(p)
+
+        for p in process_list[offset:]:
+            p.start()
+
+        time.sleep(20)
+        time.sleep(vms / 4)
+
+        used_memory = sum(result_array[0 : (vms + offset)])
+        shared = get_shared()
+        out.write(f"kata,{vms + offset},0.5,{used_memory},{shared}\n")
+        out.flush()
+        offset += vms
+
     lock.release()
 
     for p in process_list:
         p.join()
-
+    time.sleep(20)
     for e in result_array[:]:
         if e == 0:
             print("Kata failed")
             exit(-1)
-    out.write(f"kata,{n},0.5,{sum(result_array[:])}\n")
-    out.flush()
+    print("All Kata VMs were created correctly")
     print("Memory: ", sum(result_array[:]))
 
 
 def spawn_qemu(c, l, result_array, num):
     with TemporaryDirectory() as tempdir:
         qmp_socket = Path(tempdir).joinpath("qmp.sock")
-        cmd = ["sudo", "cgexec", "--sticky", "-g", "memory:vm_scale"]
+        print("Starting VM: ", num)
+        cmd = ["sudo", "cgexec", "--sticky", "-g", f"memory:vm_scale_{num}"]
         qmp_command = [
             "-qmp",
             f"unix:{str(qmp_socket)},server,nowait",
         ]
         cmd += c
         # print(cmd)
+
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         l.acquire()
         l.release()
@@ -105,50 +160,69 @@ def spawn_qemu(c, l, result_array, num):
             print("STDOUT:", p.stdout.read())
             print("STDERR:", p.stderr.read())
             return
-
+        print(f"Stopping {num}")
         p.kill()
         time.sleep(60)
         if p.poll():
             result_array[num] = True
             return
+
+        print("STDOUT:", p.stdout.read())
+        print("STDERR:", p.stderr.read())
         result_array[num] = False
 
 
-def spawn_qemu_parallel(config, n=1):
-    # Create Cgroup
-    subprocess.run(["sudo", "rmdir", "/sys/fs/cgroup/vm_scale"])
-    subprocess.run(["sudo", "mkdir", "/sys/fs/cgroup/vm_scale"])
+def spawn_qemu_parallel(config, n=[1]):
+    max_value = n[-1]
+    # Create Cgroups
+    for i in range(max_value):
+        subprocess.run(["sudo", "rmdir", f"/sys/fs/cgroup/vm_scale_{i}"])
+        subprocess.run(["sudo", "mkdir", f"/sys/fs/cgroup/vm_scale_{i}"])
 
     process_list = []
     lock = Lock()
     lock.acquire()
-    result_array = Array("i", range(n))
-    for i in range(n):
-        p = Process(target=spawn_qemu, args=(config, lock, result_array, i))
-        process_list.append(p)
+    result_array = Array("i", range(max_value))
+    offset = 0
+    start = n[0]
+    exec_list = []
+    for i in range(len(n)):
+        if i == 0:
+            exec_list.append(n[i])
+        else:
+            exec_list.append(n[i] - n[i - 1])
+    print(f"VM cycles: {exec_list}")
+    for vms in exec_list:
+        if offset != 0:
+            print(f"Starting {start} additional VMs")
+        else:
+            print(f"Starting {vms} VMs")
 
-    counter = 0
-    for p in process_list:
-        counter += 1
-        # if(counter == 20):
-        #    time.sleep(10)
-        #    counter = 0
-        p.start()
+        for i in range(offset, offset + vms, 1):
+            p = Process(target=spawn_qemu, args=(config, lock, result_array, i))
+            process_list.append(p)
+            print(i, i + offset)
+            counter = 0
+        for p in process_list[offset:]:
+            counter += 1
+            p.start()
+            if counter > 50:
+                time.sleep(15)
+                counter = 0
 
-    # Wait for all VMs to boot
-    # if(n > 100):
-    #    time.sleep(30 * (n/100))
-    time.sleep(120)
-    time.sleep(n)
+        time.sleep(60 + vms / 100)
 
-    with open("/sys/fs/cgroup/vm_scale/memory.peak", "r") as f:
-        peak_memory = int(f.read())
-    print("Memory: ", peak_memory)
-    out.write(f"{vm_type},{n},{mem},{peak_memory}\n")
-    out.flush()
+        peak_memory = 0
+        for i in range(offset + vms):
+            with open(f"/sys/fs/cgroup/vm_scale_{i}/memory.peak", "r") as f:
+                peak_memory += int(f.read())
+        print(f"Memory({vms}): ", peak_memory)
+        shared = get_shared()
+        out.write(f"{vm_type},{vms + offset},{mem},{peak_memory},{shared}\n")
+        out.flush()
+        offset += vms
     # Unlock to stop all VMs
     lock.release()
-
     for p in process_list:
         p.join()
 
@@ -179,27 +253,34 @@ def run_vm(vm="amd", mem=2, it=[]):
     cmd = [x for x in cmd if not "net0" in x]
     cmd.remove("-netdev")
     cmd.remove("-device")
+    for i in range(len(cmd)):
+        if "q35" in cmd[i]:
+            cc = cmd[i]
+            if len(cc.split("q35")[1]) == 0:
+                cmd[i] = "q35,mem-merge=on"
+            else:
+                cmd[i] = "q35,mem-merge=on," + cc.split("q35")[1]
 
-    print(" ".join(cmd))
-    extra_args = ["sudo", "cgexec", "--sticky", "-g", "memory:vm_bench"]
-    for i in it:
-        a = spawn_qemu_parallel(cmd, i)
-        if not a:
-            return False
+    a = spawn_qemu_parallel(cmd, it)
+    if not a:
+        return False
     return True
     # a.__enter__()
 
     print(r)
 
 
-for m in [0.5]:
-    mem = m
-    for i in [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]:
-        run_kata(i)
-
-    continue
-    if not run_vm("amd", m, [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]):
-        print("Failed to get data")
-        break
-
+runtime = sys.argv[1]
+match runtime:
+    case "vm":
+        vm_type = "vm"
+        run_vm("amd", 0.5, [100, 200, 300, 400, 500, 600, 700])
+    case "cvm":
+        vm_type = "cvm"
+        run_vm("snp", 0.5, [100, 200, 300, 400, 500])
+    case "kata":
+        run_kata([1, 2, 100, 200, 300, 400, 500, 600, 700])
+# run_kata(n)
+# run_vm("amd", 0.5, [100,200,300,400,500,600,700])
+# run_vm("amd",0.5, [1,2])
 out.close()
