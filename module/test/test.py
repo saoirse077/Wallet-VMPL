d567453735c3659b7c0a6fa5b96fa3a8569a6f1d
Patrick Sabanic <patrick.sabanic@tum.de>
2025-04-17 17:21:45 +0000

    Add warm to test

--- module/test/test.py
+++ module/test/test.py
@@ -11,17 +11,28 @@ with open("../../Benchmarks/SeBS/benchmarks-data/400.inference/411.image-recogni
 with open("../../Benchmarks/SeBS/benchmarks-data/400.inference/411.image-recognition/model/resnet50-0676ba61.pth", "rb") as f:
     model = f.read()
 
-input_data = {"model": model, "image": image}
-input_data = pickle.dumps(input_data)
+input_dict = {"model": model, "image": image}
+input_data = pickle.dumps(input_dict)
+
+del input_dict["model"]
+input_data2 = pickle.dumps(input_dict)
+
 with open("function.py", "rb") as f:
     func = f.read()
 
 with wallet.Wallet() as w:
     output_size = 200
     
+    start = time.time_ns()
     zygote = w.create_zygote("../libpal-image-recognition.so", "python.manifest.template", "../libsysdb-image-recognition.so")
+    end = time.time_ns()
+    print("Zygote Creation Time {(end - start) / 1e9}")
+
+    start = time.time_ns()
     trustlet_1 = zygote.create_trustlet("./function.py")
-    
+    end = time.time_ns()
+    print("Trustlet Creation Time {(end - start) / 1e9}")
+
     print("Executing first Trustlet")
 
     run_1 = trustlet_1.invoke_trustlet_bin("",0)
@@ -35,12 +46,12 @@ with wallet.Wallet() as w:
     print(f"Execution Time: {(end - start) / 1e9}")
     print(out)
 
-    trustlet_2 = zygote.create_trustlet("./function.py")
-    run_2 = trustlet_2.invoke_trustlet_bin("",0)
+    #trustlet_2 = zygote.create_trustlet("./function.py")
+    run_2 = trustlet_1.invoke_trustlet_bin("",0)
     start = time.time_ns()
-    run_2 = trustlet_2.invoke_trustlet_bin(input_data, 0)
+    run_2 = trustlet_1.invoke_trustlet_bin(input_data2, 0)
     end = time.time_ns()
-    run_2 = trustlet_2.invoke_trustlet_bin(input_data,output_size)
+    run_2 = trustlet_1.invoke_trustlet_bin("",output_size)
     
     print(f"Execution Time: {(end - start) / 1e9}")
     out = pickle.loads(run_2)
